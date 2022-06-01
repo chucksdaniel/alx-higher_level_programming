7b9d833e797c1681561e8c895e27400c4fbca019
iamdanielchukwuma@gmail.com <iamdanielchukwuma@gmail.com>
2022-06-01 03:14:59 +0100

    program that prints numbers from 0 to 99 separated by , followed by a space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number < 10:
-        print("0{},".format(number), end=" ")
-    elif number == 99:
-        print("{}".format(number))
+for number in range(100):
+    if number != 99:
+        print("{:02d},".format(number), end=" ")
     else:
-        print("{},".format(number), end=" ")
+        print("{:02d}".format(number))
